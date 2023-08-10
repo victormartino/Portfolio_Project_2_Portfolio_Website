ae6dbee252955113fe4cf359a1d28e1c8ebd3d4f
VictorMartino <martino.victor@outlook.com>
2023-08-10 06:28:37 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, redirect, url_for, flash, request, abort
+from flask_bootstrap import Bootstrap5
+
+app = Flask(__name__)
+bootstrap = Bootstrap5(app)
+
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+@app.route('/unavailable')
+def unavailable():
+    return render_template("unavailable.html")
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
